c95fae5d497a6e5352e329d8adc7ef08602f5399
mikekavouras <kavourasm+ed-1@gmail.com>
2014-05-28 23:11:14 -0400

    add my own images because people keep changing their profile photos

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
   var hidden = [
-    "https://pbs.twimg.com/profile_images/420314816444502016/xj5TnUsx_bigger.jpeg",
-    "https://pbs.twimg.com/profile_images/431418116011081728/O-fN_dvz_bigger.png",
-    "https://pbs.twimg.com/profile_images/2930313036/df4cf3bbf1796ad7cf2ccfaf03c5d5f4_bigger.jpeg",
-    "https://pbs.twimg.com/profile_images/1814308211/Shot_3-10863.jpg",
-    "https://pbs.twimg.com/profile_images/417876580124221440/R_qv1vDm_bigger.jpeg",
-    "https://pbs.twimg.com/profile_images/2745922214/a914b4566a7251b3df4ea87e0432e8d5_bigger.png",
-    "https://pbs.twimg.com/profile_images/3468757441/563cf294407c44f0e6ce0e49ae031e34_bigger.jpeg"
+    "images/yellow_orange.jpg",
   ];
 
   $('.fade').twinkle({
